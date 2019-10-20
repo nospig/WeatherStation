6810e0a591102cb737bb3f15fa5cbd4bf8f3b58a
Mark Gipson <mark@markgipson.com>
2019-10-20 20:15:14 +0700

    Move settings page back to PROGMEM.

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -71,8 +71,8 @@ void WebServer::init(SettingsManager* settingsManager)
 
     server.on("/settings.html", HTTP_GET, [](AsyncWebServerRequest *request)
     {     
-        request->send(SPIFFS, "/settings.html", String(), false, tokenProcessor);   
-        //request->send_P(200, "text/html", settings_html, tokenProcessor);
+        //request->send(SPIFFS, "/settings.html", String(), false, tokenProcessor);   
+        request->send_P(200, "text/html", settings_html, tokenProcessor);
     });
 
     server.on("/weatherSettings.html", HTTP_GET, [](AsyncWebServerRequest *request)
